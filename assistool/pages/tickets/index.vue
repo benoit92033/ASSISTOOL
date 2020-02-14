559268d14834942b6ec0d351dfe1a52876a59923
nique <nique>
2020-02-14 11:42:29 +0100

    ajout form ticket

--- assistool/pages/tickets/index.vue
+++ assistool/pages/tickets/index.vue
@@ -16,6 +16,26 @@
       </tbody>
     </table>
   </div>
+  <form action="/tickets/add" method="GET">
+    <div>
+        <p>
+            <label for="title">Titre :</label>
+            <input type="text" name="title" required>
+        </p>
+        <p>
+            <label for="priority">Priorité :</label>
+            <select name="priority" id="">
+                <option value="3">Urgent</option>
+                <option value="2">Important</option>
+                <option value="1">Corrige le batard</option>
+                <option value="0">Osef</option>
+            </select>
+        </p>
+        <p>
+            <input type="submit" value="Ajouter">
+        </p>
+    </div>
+  </form>
 </template>
 <script>
   export default {
