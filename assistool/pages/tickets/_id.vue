7be3f7adf20050c8a8229615460b8e229ff6956f
nique <nique>
2020-02-14 11:36:35 +0100

    tableau tickets

---
+++ assistool/pages/tickets/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <script>
+      tickets = [
+        {id:"0", title:"prout"},
+        {id:"1", title:"erterte"},
+        {id:"2", title:"ttre"}
+      ]
+    
+      tickets.forEach(ticket => {
+        if (id == $route.params.id)
+          let oui = ticket;
+      })
+    </script>
+    <v-forEach
+    {{oui}}
+  </div>
+</template>
