81cc8e3c003267ea4687cbdf8e18eff78d8c0702
nique <nique>
2020-02-21 14:16:18 +0100

    upd Operateur

--- assistool/pages/operateur/index.vue
+++ assistool/pages/operateur/index.vue
@@ -3,15 +3,28 @@
     <v-flex>
       <v-container fluid class="add-form">
         <v-form ref="form" v-model="valid" lazy-validation >
+          <v-flex class="d-flex">
             <v-text-field v-model="recherche" label="Rechercher un ticket"></v-text-field>
-            <v-btn color="primary" @click="validate">Rechercher</v-btn>
+            <v-btn color="primary" @click="search">Rechercher</v-btn>
+          </v-flex>
         </v-form>
       </v-container>
 
       <h1>Tickets à traiter</h1>
+      
     </v-flex>
   </v-layout>
 </template>
 <script>
-
+  export default {
+    data: () => ({
+        
+      }),
+  
+      methods: {
+        search () {
+          
+        },
+      },
+    }
 </script>
