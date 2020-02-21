75b4ef13ede8364faf3b6cbf668141717f85ab28
nique <nique>
2020-02-21 14:00:36 +0100

    Page opérateur

---
+++ assistool/pages/operateur/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <v-container fluid class="add-form">
+        <v-form ref="form" v-model="valid" lazy-validation >
+            <v-text-field v-model="recherche" label="Rechercher un ticket"></v-text-field>
+            <v-btn color="primary" @click="validate">Rechercher</v-btn>
+        </v-form>
+      </v-container>
+
+      <h1>Tickets à traiter</h1>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+
+</script>
